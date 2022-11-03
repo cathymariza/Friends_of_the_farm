81559b5d1ec8e34f710d660b7c4f385275f26473
Jonathon Qualls <jonathonq02@gmail.com>
2022-11-02 22:32:59 -0500

    Updated Nav
    - Changed Navigation _pages list to point to the class functions rather than hardcoding it in.
    - Changed all of the pages to return Column rather than Scaffold
      - This will allow things to be more dynamic
    - Switched to using the new admin.dart file

--- friends_of_the_farm/lib/profile_page.dart
+++ friends_of_the_farm/lib/profile_page.dart
@@ -5,41 +5,33 @@ class ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Profile'),
-
-      ),
-      body: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          ListTile(
-            leading: CircleAvatar(
-              backgroundColor: Theme.of(context).backgroundColor
-            ),
-            title: const Text('Sample User'),
-            subtitle: const Text('user@sample.com'),
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        ListTile(
+          leading:
+              CircleAvatar(backgroundColor: Theme.of(context).backgroundColor),
+          title: const Text('Sample User'),
+          subtitle: const Text('user@sample.com'),
+        ),
+        const Text('Your Task Groups'),
+        Padding(
+          padding: const EdgeInsets.only(left: 8.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: const [
+              ListTile(
+                leading: Icon(Icons.apple),
+                title: Text('Community Garden'),
+              ),
+              ListTile(
+                leading: Icon(Icons.catching_pokemon),
+                title: Text('Chickens'),
+              ),
+            ],
           ),
-          const Text('Your Task Groups'),
-          Padding(
-            padding: const EdgeInsets.only(left: 8.0),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: const [
-                ListTile(
-                  leading: Icon(Icons.apple),
-                  title: Text('Community Garden'),
-                ),
-                ListTile(
-                  leading: Icon(Icons.catching_pokemon),
-                  title: Text('Chickens'),
-                ),
-              ],
-            ),
-          ),
-          
-        ],
-      )
+        ),
+      ],
     );
   }
 }
