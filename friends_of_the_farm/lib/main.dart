a39a35503e8c2e9c563c03048824e16237293839
Simon <reidst@hendrix.edu>
2022-11-21 01:47:35 -0600

    Reintroduced original UI style/color
    Reintroduced the home page UI used prior to Firebase integration

--- friends_of_the_farm/lib/main.dart
+++ friends_of_the_farm/lib/main.dart
@@ -110,35 +110,68 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Welcome to Friend of the Farm'),
       ),
-      body: ListView(
-        children: <Widget>[
-          Consumer<ApplicationState>(
-            builder: (context, appState, _) => AuthFunc(
-                loggedIn: appState.loggedIn,
-                signOut: () {
-                  FirebaseAuth.instance.signOut();
-                }),
-          ),
-          // to here
-          const Divider(
-            height: 8,
-            thickness: 1,
-            indent: 8,
-            endIndent: 8,
-            color: Colors.grey,
-          ),
-          Consumer<ApplicationState>(
-            builder: (context, appState, _) => Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                if (appState.loggedIn) ...[
-                  const Header('Tasks'),
-                ],
-              ],
+      body: Column(mainAxisSize: MainAxisSize.min, children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Consumer<ApplicationState>(
+              builder: (context, appState, _) => AuthFunc(
+                  loggedIn: appState.loggedIn,
+                  signOut: () {
+                    FirebaseAuth.instance.signOut();
+                  }),
             ),
+          ],
+        ),
+        Consumer<ApplicationState>(
+          builder: (context, appState, _) => Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              if (appState.loggedIn) ...[
+                Text(
+                  'Tasks',
+                  style: GoogleFonts.lobster(fontSize: 32),
+                ),
+              ],
+            ],
           ),
-        ],
-      ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 8.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: const [
+              ListTile(
+                leading: Icon(Icons.apple),
+                title: Text('Water Garden Plots XYZ'),
+                subtitle: Text('10:30 AM'),
+              ),
+              ListTile(
+                leading: Icon(Icons.catching_pokemon),
+                title: Text('Feed the Chickens'),
+                subtitle: Text('11:00 AM'),
+              ),
+            ],
+          ),
+        ),
+        ElevatedButton(
+          key: const Key("HoursWorked"),
+          style: ElevatedButton.styleFrom(
+            textStyle: GoogleFonts.lobster(fontSize: 20),
+            backgroundColor: Colors.blueGrey
+          ),
+          child: const Text('See Hours Worked'),
+          onPressed: () {
+            showDialog(
+              context: context, 
+              builder: (context) => const AlertDialog(
+                title: Text('Hours Worked'),           // To display the title it is optional
+                content: Text('3 hours on 11/04/2022'),
+              )
+            );
+          }
+        ),
+      ]),
     );
   }
 }
@@ -235,9 +268,9 @@ class _LoginScreenState extends State<LoginScreen> {
       title: 'Friend of the Farm',
       theme: ThemeData(
         buttonTheme: Theme.of(context).buttonTheme.copyWith(
-              highlightColor: Colors.deepPurple,
+              highlightColor: Colors.blueGrey,
             ),
-        primarySwatch: Colors.deepPurple,
+        primarySwatch: Colors.blueGrey,
         textTheme: GoogleFonts.robotoTextTheme(
           Theme.of(context).textTheme,
         ),
