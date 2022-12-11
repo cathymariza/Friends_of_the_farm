8292c95412f79d85109e68ad7385bde2d8507b28
Simon <reidst@hendrix.edu>
2022-12-11 17:01:32 -0600

    Created TaskCompletion
    Clarified a distinction between tasks and task completions

--- friends_of_the_farm/lib/src/task.dart
+++ friends_of_the_farm/lib/src/task.dart
@@ -1,21 +1,12 @@
-import 'package:flutter/material.dart';
-
-enum TimeOfDay implements Comparable<TimeOfDay> {
-  morning(10),
-  midday(20),
-  evening(30),
-  night(40);
-
-  const TimeOfDay(this.timeOrder);
-  final int timeOrder;
+enum TimeOfDay {
+  morning,
+  midday,
+  evening,
+  night,
+  any;
 
   int serialize() => index;
   static TimeOfDay deserialize(int code) => TimeOfDay.values[code];
-
-  @override
-  int compareTo(TimeOfDay other) {
-    return timeOrder.compareTo(other.timeOrder);
-  }
 }
 
 enum MeasureUnits {
@@ -35,51 +26,48 @@ enum MeasureUnits {
 
 class Task {
   const Task({
-    required this.timeOfDay, 
-    required this.day, 
+    required this.description, 
+    required this.timeOfDay
+  });
+  final String description;
+  final TimeOfDay timeOfDay;
+
+  Map<String, dynamic> serialize() => {
+    "description": description, 
+    "timeOfDay": timeOfDay.serialize()};
+  static Task deserialize(Map<String, dynamic> serialization) => 
+    Task(
+      description: serialization['description'], 
+      timeOfDay: TimeOfDay.deserialize(serialization['timeOfDay'])
+    );
+}
+
+class TaskCompletion {
+  const TaskCompletion({
+    required this.time, 
+    required this.hourCount, 
     required this.quantity, 
     required this.units, 
-    required this.product});
-  final TimeOfDay timeOfDay;
-  final DateTime day;
+    required this.product
+  });
+  final DateTime time;
+  final num hourCount;
   final num quantity;
   final MeasureUnits units;
   final String product;
 
   Map<String, dynamic> serialize() => {
-    "timeOfDay": timeOfDay.serialize(), 
-    "day": day.toIso8601String(), 
+    "time": time.toIso8601String(), 
+    "hourCount": hourCount, 
     "quantity": quantity, 
     "units": units.serialize(), 
     "product": product};
-  static Task deserialize(Map<String, dynamic> serializedTask) => Task(
-    timeOfDay: TimeOfDay.deserialize(serializedTask['timeOfDay']), 
-    day: DateTime.parse(serializedTask['day']), 
-    quantity: serializedTask['quantity'], 
-    units: MeasureUnits.deserialize(serializedTask['units']), 
-    product: serializedTask['product']);
-}
-
-/// A basic view of a task, designed to easily fit into a list.
-class TaskDisplayWidget extends StatelessWidget {
-  const TaskDisplayWidget({super.key, required this.task});
-
-  final Task task;
-
-  String parseDayAsString() {
-    final months = [
-      'January', 'February', 'March', 
-      'April', 'May', 'June', 
-      'July', 'August', 'September', 
-      'October', 'November', 'December'];
-    return '${months[task.day.month]} ${task.day.day}, ${task.timeOfDay.toString()}';
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      title: Text('${task.quantity} ${task.units} of ${task.product}'),
-      subtitle: Text(parseDayAsString()),
+  static TaskCompletion deserialize(Map<String, dynamic> serialization) => 
+    TaskCompletion(
+      time: DateTime.parse(serialization['day']), 
+      hourCount: serialization['hourCount'], 
+      quantity: serialization['quantity'], 
+      units: MeasureUnits.deserialize(serialization['units']), 
+      product: serialization['product']
     );
-  }
 }
